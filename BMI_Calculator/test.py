7703e5d0a8d52ec48d7c1ce2c373cd6532990157
Juan Acevedo <jmamartinez18@gmail.com>
2024-12-18 16:29:39 -0700

    BMI Calc

---
+++ BMI_Calculator/test.py
@@ -0,0 +1,13 @@
+weight = 85
+height = 1.85
+
+bmi = weight / (height ** 2)
+
+# 🚨 Do not modify the values above
+# Write your code below 👇
+if bmi >= 25:
+    print("overweight")
+elif bmi >= 18.5:
+    print("normal weight")
+else:
+    print("underweight")
